43472fa22d9e2c53efdae8d447974b194d38ed60
Rigoberto Gonzalez Marin <rigoberto970916@gmail.com>
2022-04-03 16:38:49 -0500

    Revert "Se modifican lasclases PersonaRepo1 y PersonaRepo2 y se agrega la clase Puesto"
    This reverts commit 96e92a075b8b5cdf47e434cd38d97e047f9aa439.

--- src/main/java/com/appsrigo/ProyectoSpringBoot/repository/PersonaRepo2_Imp.java
+++ src/main/java/com/appsrigo/ProyectoSpringBoot/repository/PersonaRepo2_Imp.java
@@ -5,12 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-@Repository // Este esteriotipo se utiliza cuando la clase va a ser de acceso a datos
+@Repository
 @Qualifier("persona2") // Se agrega este esteriotipo para identificar que va a ser una segunda instancia de la clase,
 // Esto se hace por si es que hay más de una implementación de la interfaz
 public class PersonaRepo2_Imp implements IPersonaRepo {
 
-    private final Logger LOG = LoggerFactory.getLogger(PersonaRepo2_Imp.class);
+    private static Logger LOG = LoggerFactory.getLogger(PersonaRepo2_Imp.class);
 
     @Override
     public void registrar(String nombre) {
