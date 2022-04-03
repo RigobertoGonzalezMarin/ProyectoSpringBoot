96e92a075b8b5cdf47e434cd38d97e047f9aa439
Rigoberto Gonzalez Marin <rigoberto970916@gmail.com>
2022-04-03 16:19:00 -0500

    Se modifican lasclases PersonaRepo1 y PersonaRepo2 y se agrega la clase Puesto

---
+++ src/main/java/com/appsrigo/ProyectoSpringBoot/models/Puesto.java
@@ -0,0 +1,15 @@
+package com.appsrigo.ProyectoSpringBoot.models;
+
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Puesto {
+    private String nombre;
+    private String nivel;
+
+    public Puesto() {
+        // Constructor vacío
+    }
+}
